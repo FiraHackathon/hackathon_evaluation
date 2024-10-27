ab932df9027060771f5480be02d3f76b4c8aec4f
Cyrille Pierre <cyrille.pierre@inrae.fr>
2024-10-28 00:15:35 +0100

    Create project

---
+++ src/evaluation.cpp
@@ -0,0 +1,36 @@
+// Copyright 2024 INRAE, French National Research Institute for Agriculture, Food and Environment
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "hackathon_evaluation/evaluation.hpp"
+
+#include <memory>
+#include <rclcpp/node.hpp>
+
+namespace hackathon
+{
+
+Evaluation::Evaluation(const rclcpp::NodeOptions & options)
+: node_(std::make_shared<rclcpp::Node>("evaluation", options))
+{
+}
+
+rclcpp::node_interfaces::NodeBaseInterface::SharedPtr Evaluation::get_node_base_interface() const
+{
+  return node_->get_node_base_interface();
+}
+
+}  // namespace hackathon
+
+#include <rclcpp_components/register_node_macro.hpp>
+RCLCPP_COMPONENTS_REGISTER_NODE(hackathon::Evaluation)
